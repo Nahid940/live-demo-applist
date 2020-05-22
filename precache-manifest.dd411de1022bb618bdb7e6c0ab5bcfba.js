3c755cee2cb5362a4473bb8907b72f74c754a861
nahid940 <nahid940@gmail.com>
2020-05-22 11:13:27 +0600

    Updates

---
+++ precache-manifest.dd411de1022bb618bdb7e6c0ab5bcfba.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cbf592a6a6269adf432ce7393ef8e90b",
+    "url": "/live-demo-applist/index.html"
+  },
+  {
+    "revision": "47ded522e0b9bec463e0",
+    "url": "/live-demo-applist/static/css/main.32f0272f.chunk.css"
+  },
+  {
+    "revision": "b27e9c75842009b020e4",
+    "url": "/live-demo-applist/static/js/2.ba9bcb7a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/live-demo-applist/static/js/2.ba9bcb7a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "47ded522e0b9bec463e0",
+    "url": "/live-demo-applist/static/js/main.77eb38bd.chunk.js"
+  },
+  {
+    "revision": "d25619eb14f9b0ffa742",
+    "url": "/live-demo-applist/static/js/runtime-main.41c1e12b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/live-demo-applist/static/media/logo.5d5d9eef.svg"
+  }
+]);
